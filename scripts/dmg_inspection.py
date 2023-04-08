55e3f9140fe3f51dcaeea51093c4e213bd120d84
James Davis <jamesecd@gmail.com>
2023-04-08 09:43:06 +0700

    stash

--- scripts/dmg_inspection.py
+++ scripts/dmg_inspection.py
@@ -1,24 +1,3 @@
-import struct
-
-# Set the path to the encrypted .dmg file
-file_path = '/Users/jamesdavis/Documents/repos/dmg-cracker/test_resources/test.dmg'
-
-# Read the first 1024 bytes of the encrypted .dmg file to extract the header
-with open(file_path, "rb") as f:
-    header = f.read(1024)
-
-# Extract the encryption algorithm identifier, key size, and salt value from the header
-algorithm = header[2:6]
-key_size = struct.unpack(">I", header[6:10])[0]
-salt = header[14:30]
-
-# Print the extracted values
-print("Encryption algorithm:", algorithm)
-print("Key size:", key_size)
-print("Salt value:", salt)
-
-
-
 import os
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
@@ -51,5 +30,51 @@ def validate_password(password, file_path):
         return True
     else:
         return False
+
+import struct
+import io
+
+def extract_salt_and_encryption_key(dmg_path):
+    with open(dmg_path, 'rb') as dmg_file:
+        data = dmg_file.read()
+
+    # Check the DMG signature (koly)
+    signature = data[:4].decode()
+    if signature != 'koly':
+        raise ValueError("Invalid DMG signature")
+
+    # Extract the plist offset and size
+    plist_offset, plist_size = struct.unpack(">QQ", data[48:64])
+
+    # Read the plist data
+    plist_data = data[plist_offset : plist_offset + plist_size]
+
+    try:
+        import plistlib
+        plist = plistlib.loads(plist_data)
+    except Exception as e:
+        raise ValueError("Error parsing DMG plist data") from e
+
+    encrypted_key = None
+    salt = None
+
+    # Search for the encrypted key and salt in the plist
+    for resource in plist.get("resource-forks", []):
+        if "blkx" in resource:
+            blkx_data = plist["resource-forks"][resource]["blkx"]
+            encrypted_key = blkx_data.get("encrypted-key")
+            salt = blkx_data.get("salt")
+            break
+
+    if encrypted_key is None or salt is None:
+        raise ValueError("Failed to extract the encrypted key and salt from the DMG file")
+
+    return encrypted_key, salt
+
+
+dmg_path = '/Users/jamesdavis/Documents/repos/dmg-cracker/test_resources/test.dmg'
+encrypted_key, salt = extract_salt_and_encryption_key(dmg_path)
+print(f"Encrypted Key: {encrypted_key}")
+print(f"Salt: {salt}")
 
 print(validate_password("test", "/Users/jamesdavis/Documents/repos/dmg-cracker/test_resources/test.dmg"))
